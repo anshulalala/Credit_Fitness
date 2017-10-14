3d4767843bbb342fb128ec79ec2ce056064a5cd8
Anshula Singh <anshula.singh@gmail.com>
2017-10-14 03:06:08 -0700

    Working login story

--- Credit_Fitness/AppDelegate.swift
+++ Credit_Fitness/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
             FirebaseApp.configure()
+            let storyboard = UIStoryboard(name: "Login", bundle: .main)
+        
+            // 2
+            if let initialViewController = storyboard.instantiateInitialViewController() {
+            // 3
+                window?.rootViewController = initialViewController
+            // 4
+                window?.makeKeyAndVisible()
+        }
             return true
         
     }
