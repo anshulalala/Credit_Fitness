326129d2790111ab59a03f052c9eb0b0806eff21
Anshula Singh <anshula.singh@gmail.com>
2017-10-13 21:56:44 -0700

    First Commit

--- Credit_Fitness/AppDelegate.swift
+++ Credit_Fitness/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Anshula Singh on 10/13/17.
 //  Copyright © 2017 Anshula Singh. All rights reserved.
 //
-
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -15,8 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+            FirebaseApp.configure()
+            return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ Credit_Fitness/View Controllers/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Credit_Fitness
+//
+//  Created by Anshula Singh on 10/13/17.
+//  Copyright © 2017 Anshula Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
